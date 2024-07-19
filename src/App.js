26dff6268738d5fcb54331d23232cff75586abdb
Harsh Kumar <hk236091@gmail.com>
2024-07-19 16:04:00 +0530

    commit-office -final

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import UserForm from './Components/Form/UserFrom';
+//import UserForm from './Components/Form/UserFrom';
+import LoginForm from './Components/Login/LoginForm';
 
 function App() {
   return (
     <div className="App">
-      <UserForm />
+      {/* <UserForm /> */}
+      <LoginForm/>
     </div>
   );
 }
---
+++ src/Components/Login/LoginForm.js
@@ -0,0 +1,54 @@
+import './Login.css';
+import React, { useState } from 'react';
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Here you can handle form submission, e.g., send the credentials to the server
+    console.log('Username:', username);
+    console.log('Password:', password);
+    // Reset the form fields
+    setUsername('');
+    setPassword('');
+  };
+
+  return (
+    <div className='container'>
+    
+        <form onSubmit={handleSubmit}>
+        <h1>LOGIN HERE!!</h1>
+      <input
+          type="text"
+          value={username}
+          onChange={handleUsernameChange}
+          placeholder='Username *'
+          required
+        />
+      <input
+          type="password"
+          placeholder='Password *'
+          value={password}
+          onChange={handlePasswordChange}
+          required
+        />
+      <div className='btn'>
+      <button className="btns" type="submit">Submit</button>
+      <button className="btnr"  type="reset">Reset</button>
+     </div>
+    </form>
+    </div>
+  );
+};
+
+export default LoginForm;
